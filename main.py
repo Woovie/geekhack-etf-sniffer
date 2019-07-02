76b971199303e6f461e029d4465bb65e0ec20806
Jordan Banasik <woovie@woovie.net>
2019-07-02 02:48:40 +0000

    Basic working command to do the scrape

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, aiohttp, configparser, asyncio
+import discord, aiohttp, configparser, asyncio, re
 from bs4 import BeautifulSoup as bs
 
 dConfig = configparser.ConfigParser()
@@ -9,16 +9,26 @@ class discordClient(discord.Client):
         print('Logged in as ', self.user)
 
     async def on_message(self, message):
-        if message.author != self.user and message.content == 'birb!tweet':
-            await message.channel.send(content="**SQUAK!**")
+        if message.author != self.user and message.content == 'birb!getsite':
+            prettyBirb = birb()
+            siteContent = await prettyBirb.getSiteContent()
+            artisanForm = prettyBirb.checkForForm(siteContent)
+            if artisanForm:
+                await message.channel.send(content=f"<dawawd@&566828640759971860> **B I R B** {artisanForm}")
 
-async def getPkContent():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://geekhack.org/index.php?topic=79513.0') as r:
-            if r.status == 200:
-                text = await r.read()
-    return bs(text.decode('utf-8'), 'html.parser')
+class birb():
+    async def getSiteContent(a):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://geekhack.org/index.php?topic=79513.0') as r:
+                if r.status == 200:
+                    text = await r.read()
+        return bs(text.decode('utf-8'), 'html.parser')
+    def checkForForm(a, siteContent):
+        div = siteContent.find(id="msg_2048390")
+        for link in div.find_all('a'):
+            href = link.get('href')
+            if 'forms' in href:
+                return href
 
 client = discordClient()
-
 client.run(dConfig['discord']['token'])
