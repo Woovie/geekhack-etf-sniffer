3c20b0f8f0dcc592e19623b39868d02b6dc2b96c
Jordan Banasik <woovie@woovie.net>
2019-07-03 00:18:29 +0000

    Updated channel for main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-import discord, aiohttp, configparser, asyncio, re
+import discord, aiohttp, configparser, asyncio, json, datetime
 from bs4 import BeautifulSoup as bs
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 dConfig = configparser.ConfigParser()
 dConfig.read('discordconfig.ini')
+birbConfig = configparser.ConfigParser()
+birbConfig.read('birbconfig.ini')
+
+debug = True
+
+def openBirbDB():
+    with open('birbforms.json', 'r') as birblines:
+        return json.load(birblines)
+
+birbDB = openBirbDB()
 
 class discordClient(discord.Client):
     async def on_ready(self):
         print('Logged in as ', self.user)
-
-    async def on_message(self, message):
-        if message.author != self.user and message.content == 'birb!getsite':
-            prettyBirb = birb()
-            siteContent = await prettyBirb.getSiteContent()
-            artisanForm = prettyBirb.checkForForm(siteContent)
-            if artisanForm:
-                await message.channel.send(content=f"<dawawd@&566828640759971860> **B I R B** {artisanForm}")
 
 class birb():
     async def getSiteContent(a):
@@ -29,6 +32,28 @@ class birb():
             href = link.get('href')
             if 'forms' in href:
                 return href
+
+async def birbCheck():
+    global debug
+    prettyBirb = birb()
+    siteContent = await prettyBirb.getSiteContent()
+    artisanForm = prettyBirb.checkForForm(siteContent)
+    if artisanForm and not artisanForm in birbDB:
+        birbDB.append(artisanForm)
+        await client.get_channel(518088187789443072).send(content=f"<@&566828640759971860> **B I R B** {artisanForm}")
+        with open('birbforms.json', 'w') as birbout:
+            if debug:
+                print(f"[{datetime.datetime.now()}] DEBUG BIRB FOUND")
+            json.dump(birbDB, birbout)
+    elif artisanForm and artisanForm in birbDB and debug:
+            print(f"[{datetime.datetime.now()}] DEBUG old birb")
+    else:
+        if debug:
+            print(f"[{datetime.datetime.now()}] DEBUG no birb")
+
+sched = AsyncIOScheduler()
+sched.add_job(birbCheck, 'interval', seconds=10)
+sched.start()
 
 client = discordClient()
 client.run(dConfig['discord']['token'])
